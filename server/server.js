41c732f98692f10af713db21281384019a262045
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-04-08 13:27:05 -0400

    feat: logging requests

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bookRouter = require('./routes/Book');
+const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('dev'));
+
 const port = 8000
 
 app.use('/api/books', bookRouter);
