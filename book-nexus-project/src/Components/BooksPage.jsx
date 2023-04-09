6b0b068854849d352bca86613bc7521f2e692406
eloiclacomme <109964445+eloiclacomme@users.noreply.github.com>
2023-04-09 12:02:14 -0400

    Book Search Form

--- book-nexus-project/src/Components/BooksPage.jsx
+++ book-nexus-project/src/Components/BooksPage.jsx
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from 'react'
 import Book from './Book'
 import './styles.css'
 import Grid from '@mui/material/Grid';
-
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import {Accordion, AccordionSummary, AccordionDetails} from '@mui/material';
+import ExpandIcon from '@mui/icons-material/Expand';
 
 function BooksPage() {
 
@@ -18,8 +22,51 @@ function BooksPage() {
     }, []);
 
     return (
-        <div className='bookFeed'>
-            <Grid container spacing={2}>
+        <Box className='bookFeed' sx={{marginBottom: 2, width: '90%', marginX: 'auto'}}>
+            <Accordion>
+                <AccordionSummary expandIcon={<ExpandIcon />}>
+                    <h3>Book Form</h3>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <form>
+                        <Grid container spacing={2}>
+                            <Grid item xs={1}>
+                                <TextField
+                                    label="Title"
+                                    variant="outlined"
+                                />
+                                </Grid>
+                            <Grid item xs={1}>
+                                <TextField
+                                    label="Authors"
+                                    variant="outlined"
+                                />
+                                </Grid>
+                            <Grid item xs={1}>
+                                <TextField
+                                    label="Genres"
+                                    variant="outlined"
+                                />
+                            </Grid>
+                            <Grid item xs={1}>
+                                <TextField
+                                    label="Year"
+                                    variant="outlined"
+                                    type="number"
+                                    InputProps={{ inputProps: { min: 0 } }}
+                                />
+                            </Grid>
+                            <Grid item xs={2} sx={{marginTop: 1}}>
+                                <Button variant="contained" color="primary" type="submit">
+                                    Submit
+                                </Button>
+                            </Grid>
+                        </Grid>
+                        </form>
+                    </AccordionDetails>
+            </Accordion>
+
+            <Grid container spacing={2} sx={{marginTop: 2}}>
             {
                 books.map(({
                     isbn, title, year, description, image_link
@@ -36,7 +83,7 @@ function BooksPage() {
                 ))
             }
             </Grid>
-        </div>
+        </Box>
     )
 }
 
