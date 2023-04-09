396dd09861bf57abdc720c33e7088e3ca124ba88
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-04-09 17:21:39 -0400

    fix: set server port to 8000

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const server = app.listen(port, () => {
   console.log(`Server running on port ${port}!`);
 });
