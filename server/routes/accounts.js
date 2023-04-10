fd5117c88c96d3e45ee3be91fa112f1bf850c09d
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-04-10 12:51:53 -0400

    fix: google callback

--- server/routes/accounts.js
+++ server/routes/accounts.js
@@ -54,10 +54,9 @@ passport.deserializeUser(async (email, done) => {
 })
 
 router.get('/google', passport.authenticate('google', {scope: ['profile', 'email']}))
-router.get('/google/callback', passport.authenticate('google', {failureRedirect: '/login'},
-  (req, res) => {
+router.get('/google/callback', passport.authenticate('google', {failureRedirect: '/login'}), (req, res) => {
     res.redirect("/");
-  }))
+})
 
 router.get('/facebook', passport.authenticate('facebook', {scope: ['email']}))
 router.get('/facebook/callback', passport.authenticate('facebook', {successRedirect: '/', failureRedirect: '/login'}))
