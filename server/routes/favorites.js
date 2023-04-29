61d2b215e25325556b97706b05e4c9effc7148f7
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-04-29 14:32:29 -0400

    refactor: add erroring for post requests

--- server/routes/favorites.js
+++ server/routes/favorites.js
@@ -21,8 +21,10 @@ router.post('/books', async (req, res) => {
   if (!user.books.includes(req.body.book)) {
     user.books.push(req.body.book)
     await user.save()
+    res.json({ success: true, books: user.books })
+  } else {
+    res.status(400).json({message: 'Book already added'})
   }
-  res.json({books: user.books})
 })
 
 router.delete('/books', async (req, res) => {
@@ -30,13 +32,15 @@ router.delete('/books', async (req, res) => {
   if (user.books.includes(req.body.book)) {
     user.books = user.books.filter((book) => book !== req.body.book)
     await user.save()
+    res.json({ success: true, books: user.books })
+  } else {
+    res.status(400).json({message: 'Book already removed'})
   }
-  res.json({books: user.books})
 })
 
 router.get('/books/:isbn/check', async (req, res) => {
   const user = await User.findOne({email: req.user.email})
-  res.json({checked: user.books.includes(req.params.isbn)})
+  res.json({favorited: user.books.includes(req.params.isbn)})
 })
 
 router.get("/books/:isbn/count", async (req, res) => {
@@ -46,7 +50,7 @@ router.get("/books/:isbn/count", async (req, res) => {
 
 router.get('/authors', async (req, res) => {
   const user = await User.findOne({email: req.user.email})
-    res.json({authors: user.authors})
+  res.json({authors: user.authors})
 })
 
 router.post('/authors', async (req, res) => {
@@ -54,8 +58,10 @@ router.post('/authors', async (req, res) => {
   if (!user.authors.includes(req.body.author)) {
     user.authors.push(req.body.author)
     await user.save()
+    res.json({ success: true, authors: user.authors })
+  } else {
+    res.status(400).json({ message: "Author already added" })  
   }
-  res.json({authors: user.authors})
 })
 
 router.delete('/authors', async (req, res) => {
@@ -63,13 +69,15 @@ router.delete('/authors', async (req, res) => {
   if (user.authors.includes(req.body.author)) {
     user.authors = user.authors.filter((author) => author !== req.body.author)
     await user.save()
+    res.json({ authors: user.authors })
+  } else {
+    res.status(400).json({ message: "Author already removed" })
   }
-  res.json({authors: user.authors})
 })
 
 router.get("/authors/:authorId/check", async (req, res) => {
   const user = await User.findOne({ email: req.user.email })
-  res.json({ checked: user.authors.includes(req.params.authorId) })
+  res.json({ favorited: user.authors.includes(req.params.authorId) })
 })
 
 router.get("/authors/:authorId/count", async (req, res) => {
