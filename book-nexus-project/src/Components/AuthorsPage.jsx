09adc96984fabb5b2c934604d9c76911531bdbd1
eloiclacomme <109964445+eloiclacomme@users.noreply.github.com>
2023-04-29 15:09:00 -0400

    Authors Page Table

--- book-nexus-project/src/Components/AuthorsPage.jsx
+++ book-nexus-project/src/Components/AuthorsPage.jsx
@@ -2,26 +2,26 @@ import React, { useEffect, useState } from 'react'
 import Author from './Author'
 import './styles.css'
 import Grid from '@mui/material/Grid';
+import { Table, TableBody, TableCell, TableHead, TableRow, TableContainer } from '@mui/material';
+import Paper from '@mui/material/Paper';
 
+const authors = [{id: 1, name: 'uno'}, {id: 2, name: 'dos'}]
 
 function AuthorsPage() {
 
-    const [authors, setAuthors] = useState([]);
+    // const [authors, setAuthors] = useState([]);
 
     //TODO
-    useEffect(() => {
-        // fetch('/api/favorites/books')
-        fetch('/api/authors')
-            .then(res => res.json())
-            .then(data => {
-                setAuthors(data)
-            })
-            .catch(err => console.log(err))
-    }, []);
+    // useEffect(() => {
+    //     fetch('/api/recommendations/authorList')
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             setAuthors(data)
+    //         })
+    //         .catch(err => console.log(err))
+    // }, []);
 
-    return (
-        <div className='bookFeed'>
-            <Grid container spacing={2}>
+                /*{ <Grid container spacing={2}>
             {
                 authors.map(({
                     id, name
@@ -34,7 +34,31 @@ function AuthorsPage() {
                     </Grid>
                 ))
             }
-            </Grid>
+            </Grid> }*/
+
+    return (
+        <div className='bookFeed authorTable'>
+            <TableContainer component={Paper} sx={{maxWidth: '50%'}}>
+                <Table sx={{}} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="left">Ranking</TableCell>
+                            <TableCell align="left">Name</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {authors.map((author) => (
+                        <TableRow
+                        key={author.name}
+                        sx={{ }}
+                        >
+                            <TableCell align="left">{author.id}</TableCell>
+                            <TableCell align="left">{author.name}</TableCell>
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </div>
     )
 }
